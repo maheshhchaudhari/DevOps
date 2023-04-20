4030b6b8e6347e5c4ec4198c5988d1a65c94e702
maheshhchaudhari <92778991+maheshhchaudhari@users.noreply.github.com>
2023-04-20 13:56:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@
 
 resource "aws_subnet" "private_subnet" {
     vpc_id     = data.aws_vpc.vpc.id
-    cidr_block = "10.0.1.0/24"
+    cidr_block = "10.0.21.0/24"
     map_public_ip_on_launch = false
      
 
